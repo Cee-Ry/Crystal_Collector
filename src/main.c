8be10d71e2e7a3a5451d96f3688ed61da60203cc
Cee-Ry <eduaveceasar@gmail.com>
2026-06-11 19:36:22 +0800

    feat: full menu UI

--- src/main.c
+++ src/main.c
@@ -9,6 +9,12 @@ int main() {
 
   Texture2D menu_bg = LoadTexture("assets/textures/backgrounds/menu_bg.png");
   Texture2D title = LoadTexture("assets/textures/ui/titles/crystal_collector.png");
+  Texture2D menu[4];
+
+  menu[0] = LoadTexture("assets/textures/ui/buttons/play.png");
+  menu[1] = LoadTexture("assets/textures/ui/buttons/settings.png");
+  menu[2] = LoadTexture("assets/textures/ui/buttons/credits.png");
+  menu[3] = LoadTexture("assets/textures/ui/buttons/exit.png");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -32,9 +38,16 @@ int main() {
 
     DrawTexture(title, ((WIDTH / 2) - (title.width / 2)), 25, WHITE);
 
+    int distance = 0;
+    for (int i = 0; i < 4; i++) {
+      DrawTexture(menu[i], ((WIDTH / 2) - (menu[i].width / 2)), (HEIGHT / 2) + distance, WHITE);
+      distance += 90;
+    }
+
     EndDrawing();
   }
 
+  for (int i = 0; i < 4; i++) UnloadTexture(menu[i]);
   UnloadTexture(title);
   UnloadTexture(menu_bg);
   CloseWindow();

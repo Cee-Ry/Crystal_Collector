1ac9f2db9b388a71f22c32df583f404386d4fc94
Cee-Ry <eduaveceasar@gmail.com>
2026-06-11 17:50:46 +0800

    feat: Menu Background

--- src/main.c
+++ src/main.c
@@ -3,8 +3,19 @@
 int main() {
   InitWindow(1280, 720, "CRYSTAL COLLECTOR");
 
+  Texture2D menu_bg = LoadTexture("assets/textures/backgrounds/menu_bg.png");
+
   while (!WindowShouldClose()) {
     BeginDrawing();
+
+    DrawTexturePro(
+      menu_bg,
+      (Rectangle){ 0, 0, menu_bg.width, menu_bg.height }, 
+      (Rectangle){ 0, 0, GetScreenWidth(), GetScreenHeight() },
+      (Vector2){ 0, 0 },  
+      0.0f,              
+      WHITE             
+    );
 
     EndDrawing();
   }

6147a59c9fcf437040b62c38c098121de866f4ac
Cee-Ry <eduaveceasar@gmail.com>
2026-06-11 18:57:40 +0800

    feat: title and dimming the background image

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include "raylib.h"
 
 int main() {
-  InitWindow(1280, 720, "CRYSTAL COLLECTOR");
+  const int WIDTH = 1280;
+  const int HEIGHT = 720;
+
+  InitWindow(WIDTH, HEIGHT, "CRYSTAL COLLECTOR");
+  SetTargetFPS(60);
 
   Texture2D menu_bg = LoadTexture("assets/textures/backgrounds/menu_bg.png");
+  Texture2D title = LoadTexture("assets/textures/ui/titles/crystal_collector.png");
+  Texture2D dim = LoadTexture("assets/textures/ui/effects/dim_effect_full.png");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -17,9 +23,23 @@ int main() {
       WHITE             
     );
 
+    DrawTexturePro(
+      dim,
+      (Rectangle){ 0, 0, dim.width, dim.height }, 
+      (Rectangle){ 0, 0, GetScreenWidth(), GetScreenHeight() },
+      (Vector2){ 0, 0 },  
+      0.0f,              
+      WHITE             
+    );
+
+    DrawTexture(title, ((WIDTH / 2) - (title.width / 2)), 25, WHITE);
+
     EndDrawing();
   }
 
+  UnloadTexture(dim);
+  UnloadTexture(title);
+  UnloadTexture(menu_bg);
   CloseWindow();
   return 0;
 }
